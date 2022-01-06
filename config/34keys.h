1fef2cda25e7da209df5e7d8545a749aff797b6a
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2022-01-06 21:50:09 +0100

    Add keymap for ferris sweep, remove kyria

---
+++ config/34keys.h
@@ -0,0 +1,230 @@
+#define NONE____ &none
+#define ________ &trans
+
+#define DK_A____ &hm LCTRL  _DK_A____
+#define DK_B____ &kp        _DK_B____
+#define DK_C____ &kp        _DK_C____
+#define DK_D____ &kp        _DK_D____
+#define DK_E____ &hm LSHFT  _DK_E____
+#define DK_F____ &kp        _DK_F____
+#define DK_G____ &kp        _DK_G____
+#define DK_H____ &kp        _DK_H____
+#define DK_I____ &hm LALT   _DK_I____
+#define DK_J____ &kp        _DK_J____
+#define DK_K____ &kp        _DK_K____
+#define DK_L____ &kp        _DK_L____
+#define DK_M____ &kp        _DK_M____
+#define DK_N____ &hm LGUI   _DK_N____
+#define DK_O____ &hm LCTRL  _DK_O____
+#define DK_P____ &kp        _DK_P____
+#define DK_Q____ &kp        _DK_Q____
+#define DK_R____ &hm LALT   _DK_R____
+#define DK_S____ &hm LSHFT  _DK_S____
+#define DK_T____ &hm LGUI   _DK_T____
+#define DK_U____ &kp        _DK_U____
+#define DK_V____ &kp        _DK_V____
+#define DK_W____ &kp        _DK_W____
+#define DK_X____ &kp        _DK_X____
+#define DK_Y____ &kp        _DK_Y____
+#define DK_Z____ &kp        _DK_Z____
+#define DK_AE___ &kp        _DK_AE___
+#define DK_OE___ &kp        _DK_OE___
+#define DK_AA___ &kp        _DK_AA___
+
+#define DK_DLLR_ &kp        _DK_DLLR_
+#define DK_1____ &kp        _DK_1____
+#define DK_2____ &kp        _DK_2____
+#define DK_3____ &kp        _DK_3____
+#define DK_4____ &kp        _DK_4____
+#define DK_5____ &kp        _DK_5____
+#define DK_6____ &kp        _DK_6____
+#define DK_7____ &kp        _DK_7____
+#define DK_8____ &kp        _DK_8____
+#define DK_9____ &kp        _DK_9____
+#define DK_0____ &kp        _DK_0____
+#define DK_PLUS_ &kp        _DK_PLUS_
+#define DK_ACUTE &kp        _DK_ACUTE
+
+#define DK_F1___ &kp        _DK_F1___
+#define DK_F2___ &kp        _DK_F2___
+#define DK_F3___ &kp        _DK_F3___
+#define DK_F4___ &kp        _DK_F4___
+#define DK_F5___ &kp        _DK_F5___
+#define DK_F6___ &kp        _DK_F6___
+#define DK_F7___ &kp        _DK_F7___
+#define DK_F8___ &kp        _DK_F8___
+#define DK_F9___ &kp        _DK_F9___
+#define DK_F10__ &kp        _DK_F10__
+#define DK_F11__ &kp        _DK_F11__
+#define DK_F12__ &kp        _DK_F12__
+
+#define DK_SECT_ &kp        _DK_SECT_ 
+#define DK_EXCL_ &kp        _DK_EXCL_ 
+#define DK_DQT__ &kp        _DK_DQT__ 
+#define DK_HASH_ &kp        _DK_HASH_ 
+#define DK_EURO_ &kp        _DK_EURO_ 
+#define DK_PERC_ &kp        _DK_PERC_ 
+#define DK_AMPS_ &kp        _DK_AMPS_ 
+#define DK_SLASH &kp        _DK_SLASH 
+#define DK_LPAR_ &kp        _DK_LPAR_ 
+#define DK_RPAR_ &kp        _DK_RPAR_ 
+#define DK_EQUAL &kp        _DK_EQUAL 
+#define DK_QMARK &kp        _DK_QMARK 
+#define DK_GRAVE &kp        _DK_GRAVE 
+
+#define DK_APPLE &kp        _DK_APPLE 
+#define DK_R_EXL &kp        _DK_R_EXL 
+#define DK_POUND &kp        _DK_POUND 
+#define DK_INFIN &kp        _DK_INFIN 
+#define DK_TM___ &kp        _DK_TM___ 
+#define DK_LBRC_ &kp        _DK_LBRC_ 
+#define DK_RBRC_ &kp        _DK_RBRC_ 
+#define DK_DIFF_ &kp        _DK_DIFF_ 
+
+#define DK_PERM_ &kp        _DK_PERM_ 
+#define DK_BSLSH &kp        _DK_BSLSH 
+#define DK_LCBRC &kp        _DK_LCBRC 
+#define DK_RCBRC &kp        _DK_RCBRC 
+#define DK_AL_EQ &kp        _DK_AL_EQ 
+#define DK_R_QST &kp        _DK_R_QST 
+#define DK_ACC_N &kp        _DK_ACC_N 
+
+// Other symbols
+#define DK_LS_TH &kp        _DK_LS_TH 
+#define DK_GT_TH &kp        _DK_GT_TH 
+#define DK_LT_EQ &kp        _DK_LT_EQ 
+#define DK_GT_EQ &kp        _DK_GT_EQ 
+
+#define DK_COMMA &kp        _DK_COMMA 
+#define DK_SCLN_ &kp        _DK_SCLN_ 
+
+#define DK_DOT__ &kp        _DK_DOT__ 
+#define DK_COLON &kp        _DK_COLON 
+
+#define DK_MINUS &kp        _DK_MINUS 
+#define DK_UNDS_ &kp        _DK_UNDS_ 
+
+#define DK_SQT__ &kp        _DK_SQT__ 
+#define DK_ASTR_ &kp        _DK_ASTR_ 
+#define DK_AT___ &kp        _DK_AT___ 
+#define DK_DGREE &kp        _DK_DGREE 
+
+#define DK_DIAC_ &kp        _DK_DIAC_
+#define DK_TILDE &kp        _DK_TILDE
+#define DK_CARET &kp        _DK_CARET
+
+#define DK_PIPE_ &kp        _DK_PIPE_ 
+
+#define DK_ESC__ &kp        _DK_ESC__
+#define DK_TAB__ &kp        _DK_TAB__
+#define DK_STAB_ &kp        _DK_STAB_
+#define DK_SHIFT &kp        _DK_SHIFT
+#define DK_CTRL_ &kp        _DK_CTRL_
+#define DK_ALT__ &kp        _DK_ALT__
+#define DK_GUI__ &kp        _DK_GUI__
+#define DK_SPACE &kp        _DK_SPACE
+#define DK_ENTER &kp        _DK_ENTER
+#define DK_BSPC_ &kp        _DK_BSPC_
+#define DK_DEL__ &kp        _DK_DEL__
+
+#define DK_UP___ &kp        _DK_UP___
+#define DK_DOWN_ &kp        _DK_DOWN_
+#define DK_LEFT_ &kp        _DK_LEFT_
+#define DK_RIGHT &kp        _DK_RIGHT
+#define DK_PG_UP &kp        _DK_PG_UP
+#define DK_PG_DN &kp        _DK_PG_DN
+#define DK_HOME_ &kp        _DK_HOME_
+#define DK_END__ &kp        _DK_END__
+
+#define DK_INSRT &kp        _DK_INSRT
+
+#define DK_UNDO_ &kp        _DK_UNDO_
+#define DK_CUT__ &kp        _DK_CUT__
+#define DK_COPY_ &kp        _DK_COPY_
+#define DK_PASTE &kp        _DK_PASTE
+
+#define DK_VOLMT &kp        _DK_VOLMT
+#define DK_VOLUP &kp        _DK_VOLUP
+#define DK_VOLDN &kp        _DK_VOLDN
+#define DK_PLAY_ &kp        _DK_PLAY_
+#define DK_PREV_ &kp        _DK_PREV_
+#define DK_NEXT_ &kp        _DK_NEXT_
+
+// Layers
+#define _COL                0 // Colemak DH
+#define _NAV                1 // Navigation
+#define _NUM                2 // Numpad
+#define _SYM                3 // Symbols
+#define _SYS                4 // System
+#define _FUN                5 // Funpad
+
+// Thumb keys, two on each hand, left to right
+#define SYM_TAB_ &lt        _SYM _DK_TAB__
+#define NAV_SPC_ &lt        _NAV _DK_SPACE
+#define NUM_BSPC &lt        _NUM _DK_BSPC_
+#define SYM_DEL_ &lt        _SYM _DK_DEL__
+// On the numpad layer, NAV_SPC_ is overridden to output zero
+#define NAV_ZERO &lt        _NAV _DK_0____
+
+// All shortcuts for Magnet
+#define MG_FSCRN &kp        MAGNET_FULLSCREEN
+#define MG_LT_HF &kp        MAGNET_LEFT_HALF
+#define MG_RT_HF &kp        MAGNET_RIGHT_HALF
+#define MG_UP_HF &kp        MAGNET_UPPER_HALF
+#define MG_BM_HF &kp        MAGNET_BOTTOM_HALF
+
+#define MG_LT3RD &kp        MAGNET_LEFT_THIRD
+#define MG_MD3RD &kp        MAGNET_MIDDLE_THIRD
+#define MG_RT3RD &kp        MAGNET_RIGHT_THIRD
+
+#define MG_UL_QT &kp        MAGNET_UPPER_LEFT_QUADRANT
+#define MG_UR_QT &kp        MAGNET_UPPER_RIGHT_QUADRANT
+#define MG_BL_QT &kp        MAGNET_BOTTOM_LEFT_QUADRANT
+#define MG_BR_QT &kp        MAGNET_BOTTOM_RIGHT_QUADRANT
+
+#define MG_L23RD &kp        MAGNET_LEFT_TWO_THIRDS
+#define MG_R23RD &kp        MAGNET_RIGHT_TWO_THIRDS
+
+// Capsword
+#define CAPSWORD &capsword
+
+// One Shot Shift
+#define SHIFT___ &sk        _DK_SHIFT
+
+// clipboard
+#define UNDO____ &kp        _UNDO
+#define CUT_____ &kp        _CUT
+#define COPY____ &kp        _COPY
+#define PASTE___ &kp        _PASTE
+
+// General shortcuts
+#define NEXT_TAB &kp        _NEXT_TAB
+#define PREV_TAB &kp        _PREV_TAB
+#define NXT_DSKT &kp        _NEXT_DESKTOP
+#define PRV_DSKT &kp        _PREV_DESKTOP
+
+#define SCRNSHOT &kp        _SCREENSHOT
+#define LOCK_MAC &kp        _LOCK_COMPUTER
+#define ALFRED__ &kp        _ALFRED 
+#define BSPC_WRD &kp        _BACKSPACE_ONE_WORD 
+#define DEL_WRD_ &kp        _DELETE_ONE_WORD
+
+#define COPY_T__ &kp        _COPY_TERMINAL
+#define PASTE_T_ &kp        _PASTE_TERMINAL
+
+// reset and bootload
+#define RESET___ &reset
+#define BOOTLOAD &bootloader
+
+// bluetooth
+#define BT_CLEAR &bt        BT_CLR
+#define BT_SEL_1 &bt        BT_SEL 0
+#define BT_SEL_2 &bt        BT_SEL 1
+#define BT_SEL_3 &bt        BT_SEL 2
+#define BT_NEXT_ &bt        BT_NXT
+#define BT_PREV_ &bt        BT_PRV
+
+// output
+#define OUT_USB_ &out       OUT_USB
+#define OUT_BLE_ &out       OUT_BLE
+#define OUT_TOG_ &out       OUT_TOG 
